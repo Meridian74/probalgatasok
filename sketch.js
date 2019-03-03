8e5b441141b7d213f3dc6c0ef0fd3acea8c5565c
Meridian74 <meridian@freemail.hu>
2019-03-03 16:34:45 +0100

    feat:  színezés pixelenként

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-let SCREEN_W = 640;
-let SCREEN_H = 400;
+let SCREEN_W = 256;
+let SCREEN_H = 256;
 var x;
 var y;
 var i;
@@ -9,15 +9,15 @@ function setup() {
    background(0);
    loadPixels();
    let d = pixelDensity();
-   let pixRows = SCREEN_W * 4 * d;  // egy sor ennyi alpixelből áll valójában
+   let pixRows = SCREEN_W * d * 4;  // egy sor ennyi alpixelből áll valójában
    let pixRolls = SCREEN_H * d;     // egy oszlop ennyi sorból áll valójában
    for (i = 0; i < pixRolls; i++) {
       y = i * SCREEN_W * 4 * d;
       for (x = 0; x < pixRows; x += 4) {
-         pixels[x + y + 0] = 200;
-         pixels[x + y + 1] = random(255);
-         pixels[x + y + 2] = random(100,200);
-         pixels[x + y + 3] = 255;
+         pixels[x + y + 0] = 0;           // R color level
+         pixels[x + y + 1] = x / 4 / d;   // G color level
+         pixels[x + y + 2] = i / d;       // B color level
+         pixels[x + y + 3] = 255;         // Alpha level
       }
    }
    updatePixels();
