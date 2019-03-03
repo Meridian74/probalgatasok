417030b57187e75fc1ef60d8fa111118202d9e98
Meridian74 <meridian@freemail.hu>
2019-03-03 16:25:26 +0100

    feat: képernyő pixelmanipuláció

--- sketch.js
+++ sketch.js
@@ -1,57 +1,30 @@
-var screenWidth = 640;
-var screenHeight = 400;
-var r = 0;
-var x = 0;
-var y = 0;
-var dX = 0;
-var dY = 0;
-var step = 0;
+let SCREEN_W = 640;
+let SCREEN_H = 400;
+var x;
+var y;
+var i;
 
 function setup() {
-  createCanvas(screenWidth, screenHeight);
-  background(50);
-  var x = 0.0 + random(screenWidth - r/2);
-  var y = 0.0 + random(screenHeight - r/2);
-  dX = random(-8,8);
-  dY = random(-8,8);
-  step = parseInt(random(20));
-  r = 20;
+   createCanvas(SCREEN_W, SCREEN_H);
+   background(0);
+   loadPixels();
+   let d = pixelDensity();
+   let pixRows = SCREEN_W * 4 * d;  // egy sor ennyi alpixelből áll valójában
+   let pixRolls = SCREEN_H * d;     // egy oszlop ennyi sorból áll valójában
+   for (i = 0; i < pixRolls; i++) {
+      y = i * SCREEN_W * 4 * d;
+      for (x = 0; x < pixRows; x += 4) {
+         pixels[x + y + 0] = 200;
+         pixels[x + y + 1] = random(255);
+         pixels[x + y + 2] = random(100,200);
+         pixels[x + y + 3] = 255;
+      }
+   }
+   updatePixels();
 }
+
 
 function draw() {
-  
+   // background(0);
 
-  fill(255);
-  ellipse(x, y, r, r);
-
-  x = x + dX;
-  y = y + dY;
-  if (step > 0) {
-    step = step - 1;
-  }
-  else {
-    step = parseInt(random(20));
-    dX = random(-8,8);
-    dY = random(-8,8);
-  }
-
-  if (x < (r/2)) {
-    x = r/2 - 1;
-    dX = dX * -1;
-  }
-  else if (x > (screenWidth - r/2)) {
-    x = screenWidth - r/2;
-    dX = dX * -1;
-  }
-
-  if (y < (r/2)) {
-    y = r/2;
-    dY = dY * -1;
-  }
-  else if (y > (screenHeight - r/2)) {
-    y = screenHeight - r/2;
-    dY = dY * -1;
-  }
-  
 }
-
